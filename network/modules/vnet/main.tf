94d46a843d8e6d2e2d5a1a0d18f2a517f134b667
rice1gou <rice1gou@gmail.com>
2022-08-18 18:19:45 +0900

    create vnet modules #8

--- network/modules/vnet/main.tf
+++ network/modules/vnet/main.tf
@@ -1,3 +1,9 @@
+locals {
+  subnets = ["psql", "st", "k8s"]
+  count = 3
+}
+
+#Create Resource Group
 resource "azurerm_resource_group" "network" {
   name     = var.resource_group_name
   location = var.location
@@ -9,4 +15,96 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = var.address_space
   location            = var.location
   resource_group_name = var.resource_group_name
+}
+
+#Create Subnet
+resource "azurerm_subnet" "ds" {
+  name = "${var.name_prefix}-subnet-ds"
+  resource_group_name = azurerm_resource_group.network.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_space = cidrsubnet(azurerm_virtual_network.vnet.address_space[0], 8, 1)
+}
+
+resource "azurerm_subnet" "psql" {
+  name = "${var.name_prefix}-subnet-psql"
+  resource_group_name = azurerm_resource_group.network.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_space = cidrsubnet(azurerm_virtual_network.vnet.address_space[0], 8, 2)
+}
+
+resource "azurerm_subnet" "k8s" {
+  name = "${var.name_prefix}-subnet-k8s"
+  resource_group_name = azurerm_resource_group.network.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_space = cidrsubnet(azurerm_virtual_network.vnet.address_space[0], 8, 3)
+}
+
+#Create NSG
+resource "azurerm_network_security_group" "ds" {
+  name = "${var.name_prefix}-nsg-ds"
+  location = var.location
+  resource_group_name = azurerm_resource_group.network.name
+
+  security_rule {
+    name                       = "nsg-ds-rule"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_security_group" "psql" {
+  name = "${var.name_prefix}-nsg-psql"
+  location = var.location
+  resource_group_name = azurerm_resource_group.network.name
+
+  security_rule {
+    name                       = "nsg-psql-rule"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+resource "azurerm_network_security_group" "k8s" {
+  name = "${var.name_prefix}-nsg-k8s"
+  location = var.location
+  resource_group_name = azurerm_resource_group.network.name
+
+  security_rule {
+    name                       = "nsg-k8s-rule"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+#Create Subnet NSG Association
+resource "azurerm_subnet_network_security_group_association" "ds" {
+  subnet_id = azurerm_subnet.ds.id
+  network_security_group_id = azurerm_network_security_group.ds.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "psql" {
+  subnet_id = azurerm_subnet.psql.id
+  network_security_group_id = azurerm_network_security_group.psql.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "k8s" {
+  subnet_id = azurerm_subnet.k8s.id
+  network_security_group_id = azurerm_network_security_group.k8s.id
 }
