f6060477592a50c9127c840a39260a7ebd1a3ddf
rice1gou <rice1gou@gmail.com>
2022-08-18 13:00:45 +0900

    change backend azure #6

--- network/envs/develop/main.tf
+++ network/envs/develop/main.tf
@@ -1,28 +1,29 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.18.0"
+    source  = "hashicorp/azurerm"
+    version = "=3.18.0"
     }
+  }
+
+  backend "azurerm" {
   }
 }
 
 provider "azurerm" {
   features {}
+  skip_provider_registration = true
 }
 
-#
-# 内部変数
-#
 locals {
-  name_prefix = "dev"
+  name_prefix                 = "dev"
 
 }
 
 module "vnet" {
-  source              = "../../modules/vnet"
+  source = "../../modules/vnet"
   resource_group_name = var.network_resource_group_name
-  location            = var.location
-  name_prefix         = local.name_prefix
-  address_space       = var.address_space
+  location = var.location
+  name_prefix = local.name_prefix
+  address_space = var.address_space
 }
