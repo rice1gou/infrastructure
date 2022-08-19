b6ee6ad19ac6368fb2506f19d246b2db66423e70
rice1gou <rice1gou@gmail.com>
2022-08-19 11:46:49 +0900

    Fixing Dependencies #14

--- base/modules/vnet/main.tf
+++ base/modules/vnet/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "${var.name_prefix}-vnet"
   address_space       = var.address_space
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.network.name
 }
 
 #Create Subnet
