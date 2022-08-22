eb95b42de63a054290ae52637130b711e0902e5b
rice1gou <rice1gou@gmail.com>
2022-08-22 17:46:01 +0900

    change kv name #17

--- base/modules/kv/main.tf
+++ base/modules/kv/main.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "kv" {
-  name                        = "${var.name_prefix}-app-kv"
+  name                        = "${var.name_prefix}-base-kv"
   location                    = var.location
   resource_group_name         = var.resource_group_name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
