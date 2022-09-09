713c5198e5b79f0cc987e6bf0207b0d77cd1d7a1
rice1gou <rice1gou@gmail.com>
2022-09-09 22:41:50 +0900

    workflow setup #28

--- container/envs/local/bash/uninstall-istio.sh
+++ container/envs/local/bash/uninstall-istio.sh
@@ -6,4 +6,4 @@ ISTIO_VERSION=1.15.0
 
 kubectl label namespace default istio-injection-
 
-./istio-${ISTIO_VERSION}/bin/istioctl x uninstall --purge
+./istio-${ISTIO_VERSION}/bin/istioctl uninstall -y --purge
